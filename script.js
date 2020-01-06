4487aaaf68bbe389e03651056be06ff2c0b0a392
Afeez Awoyemi <damafeez@gmail.com>
2020-01-06 23:42:02 +0100

    refactor(logic): refactor script to more functional approach
    - remove svg icon
    - fix not parsing 0.decimal bug

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const buttons = document.querySelectorAll('.controls > span')
 const calculation = []
 let result = null
 const maxResultLength = 9
+const maxCalculationLength = 20
+const lastInputIsOperator = inputs => isOperator(inputs[inputs.length - 1])
+const operatorIsRedundant = (inputs, value) => isOperator(value) && inputs.length && lastInputIsOperator(inputs)
+const contains = (list, value) => list.findIndex(element => element === value) > -1
 const specialOperators = {
   '='() {
     calculation.length = 0
@@ -15,9 +19,8 @@ const specialOperators = {
     calculation.pop()
   },
   '.'() {
-    const hasDot = calculation.findIndex(value => value === '.') > -1
     if (!calculation.length) calculation.push('0', '.')
-    else if (!hasDot) calculation.push('.')
+    else if (!contains(calculation, '.')) calculation.push('.')
   }
 }
 buttons.forEach(button => button.addEventListener('click', onClick))
@@ -27,14 +30,13 @@ function onClick(event) {
   const target = event.currentTarget
   const proxy = target.getAttribute('data-proxy')
   const value = proxy ? proxy : target.innerText
-  const lastIsOperator = isOperator(calculation[calculation.length - 1])
 
-  if (isOperator(value) && !calculation.length) return
-  else if (isOperator(value) && lastIsOperator) calculation[calculation.length - 1] = value
-  else if (isSpecialOperator(value)) specialOperators[value]()
-  else if (isNumber(value) || isOperator(value)) calculation.push(value)
+  if (isSpecialOperator(value)) specialOperators[value]()
+  else if (calculation.length >= maxCalculationLength) return alert('Max calculation Reached!')
+  else if (operatorIsRedundant(calculation, value)) calculation[calculation.length - 1] = value
+  else if (isOperator(value) || isNumber(value)) calculation.push(value)
 
-  result = calculator(calculation)
+  result = lastInputIsOperator(calculation) ? null : calculator(calculation)
   render()
 }
 function render() {
@@ -52,18 +54,18 @@ function calculator(calculation) {
 
 function isOperator(value) {
   const operators = ['x', '/', '+', '-']
-  return operators.findIndex(operator => operator === value) > -1
+  return contains(operators, value)
 }
 function isSpecialOperator(value) {
-  return Object.keys(specialOperators).findIndex(operator => operator === value) > -1
+  return contains(Object.keys(specialOperators), value)
 }
 function isNumber(value) {
-  const specialNumbers = ['0', '00', 0]
-  return specialNumbers.findIndex(number => number === value) > -1 || Number(value) > 0
+  const specialNumbers = ['0', 0]
+  return contains(specialNumbers, value) || !!parseFloat(value)
 }
 function parseResult(result) {
   if (!isNumber(result)) return ''
-  const _result = result.toPrecision(maxResultLength).toString()
+  const _result = result.toPrecision(maxResultLength)
   const sliced = _result.length > maxResultLength ? _result.slice(0, maxResultLength) : _result
-  return Number(sliced)
+  return parseFloat(sliced)
 }
